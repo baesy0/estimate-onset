ad5336a87dd92ca97505ef1d2d1d890610e794c0
Bailey <bsy2658@gmail.com>
2019-10-10 21:05:50 +0900

    바뀐 파일명 적용

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aws s3 cp ./estimate-onset.html s3://75mm.studio --acl public-read --profile 75mm.studio
-aws s3 cp ./js/estimate-onset.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
-aws s3 cp ./css/estimate-onset.css s3://75mm.studio/css/ --acl public-read --profile 75mm.studio
-aws s3 cp ./img/estimateicon-onset.png s3://75mm.studio/img/ --acl public-read --profile 75mm.studio
+aws s3 cp ./onset-pricing-eng.html s3://75mm.studio --acl public-read --profile 75mm.studio
+aws s3 cp ./js/onset-pricing.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./css/onset-pricing.css s3://75mm.studio/css/ --acl public-read --profile 75mm.studio
+aws s3 cp ./img/onset-pricingicon.png s3://75mm.studio/img/ --acl public-read --profile 75mm.studio
