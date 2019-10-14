c7f68379f8e92dd6035c686b0faddf6dfdfe9e5d
Bailey <bsy2658@gmail.com>
2019-10-14 21:18:39 +0900

    publish.sh 수정

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 aws s3 cp ./onset-pricing-eng.html s3://75mm.studio --acl public-read --profile 75mm.studio
 aws s3 cp ./onset-pricing-kor.html s3://75mm.studio --acl public-read --profile 75mm.studio
-aws s3 cp ./js/onset-pricing.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./js/onset-pricing-eng.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./js/onset-pricing-kor.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
 aws s3 cp ./css/onset-pricing.css s3://75mm.studio/css/ --acl public-read --profile 75mm.studio
 aws s3 cp ./img/onset-pricingicon.png s3://75mm.studio/img/ --acl public-read --profile 75mm.studio
